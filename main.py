0ea9439642cfe651ad8db75358b4b6ce436667cd
KinfThaDerp <bassam.grzegorz@gmail.com>
2025-10-29 09:42:46 +0100

    wip: list of names, locations, posts

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ zmienna_1:str = "Bassam"
 zmienna_2:str = "Michasia"
 zmienna_3:str = "Kasia"
 zmienna_4:str = "Asia"
+# Przygotuj liste slowników które będą przechowywały informacje o użytkownikach na temat:
+# Imie(alias name), Miejscowość (location), Liczba postów(posts),
+users:list=[
+    {"alias_name":zmienna_1, "location":"Warszawa", "posts":10},
+    {"alias_name":zmienna_2, "location":"Radom", "posts":20},
+    {"alias_name":zmienna_3, "location":"Legionowo", "posts":30},
+    {"alias_name":zmienna_4, "location":"Milanówek", "posts":40}
+]
 
-uzytkownicy:list=[zmienna_1,zmienna_2,zmienna_3,zmienna_4]
-
-for uzytkownik in uzytkownicy:
-    print(f"Hello {uzytkownik}")
+for user in users:
+    print(f"Twój znajomy {user["alias_name"]} z miejscowości {user["location"]} opublikował {user['posts']} postów.")
