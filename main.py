7e735dabcadb99c76e2537994fad30032793d34a
KinfThaDerp <bassam.grzegorz@gmail.com>
2025-10-29 09:10:15 +0100

    wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+zmienna_1:str = "Bassam"
+zmienna_2:str = "Michasia"
+zmienna_3:str = "Kasia"
+zmienna_4:str = "Asia"
+
+uzytkownicy:list=[zmienna_1,zmienna_2,zmienna_3,zmienna_4]
+
+for uzytkownik in uzytkownicy:
+    print(f"Hello {uzytkownik}")
